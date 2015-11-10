d731fdf15ddbf539eef5d76318231da930d8d5eb
unknown <Nancy.Liang@Nancy-PC.ef.com>
2015-11-10 11:42:39 +0800

    first case

--- Prime/Prime/TestPrime.cs
+++ Prime/Prime/TestPrime.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Prime
@@ -6,9 +8,20 @@ namespace Prime
     [TestClass]
     public class TestPrime
     {
+        //AAA:Arrange, Action, Assert
+        //Readable name
         [TestMethod]
-        public void TestMethod1()
+        public void return_empty_for_1()
         {
+            CollectionAssert.AreEqual(new List<int>(), PrimeFactor.Generate(1));
+        }
+    }
+
+    public class PrimeFactor
+    {
+        public static List<int> Generate(int input)
+        {
+            return new List<int>();
         }
     }
 }
