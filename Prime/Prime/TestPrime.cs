33587d51249100c17a2b088b9db1133236ec0c2f
unknown <Nancy.Liang@Nancy-PC.ef.com>
2015-11-10 10:49:05 +0800

    new solution

---
+++ Prime/Prime/TestPrime.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Prime
+{
+    [TestClass]
+    public class TestPrime
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
